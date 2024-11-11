0e3e13137f94928f20d23d26c0cb7eac1267247f
Nguyen Xuan Tam <nguyenxuantam2611@gmail.com>
2024-11-11 10:58:28 +0700

    remove commented-out console log in database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH, MONGO_DB, MONGO_DB_APP_NAME } =
   process.env;
-console.log(
-  `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
-);
+// console.log(
+//   `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
+// );
 const connection = mongoose.connect(
   `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
 );
