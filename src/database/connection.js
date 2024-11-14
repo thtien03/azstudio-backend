03c16f2ee4d746bc95d3beb59d3c91fe2772cc43
thtien03 <118674266+thtien03@users.noreply.github.com>
2024-11-14 22:15:11 +0700

    update

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH, MONGO_DB, MONGO_DB_APP_NAME } =
   process.env;
-// console.log(
-//   `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
-// );
-const connection = mongoose.connect(
+console.log(
   `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
+);
+const connection = mongoose.connect(
+  `mongodb://${MONGO_PATH}/${MONGO_DB}`
 );
 
 export default connection;
--- src/router/service.routes.js
+++ src/router/service.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/", ServiceController.getAllServices);
 router.post("/", ServiceController.createService);
-router.put("/:id", auth, ServiceController.updateService);
+router.put("/:id", ServiceController.updateService);
 router.delete("/:id", auth, ServiceController.deleteService);
 
 export default router;
