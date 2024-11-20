63a2b81fa5ba566a424c8e5a69f0bbbc450cd153
Nguyen Xuan Tam <nguyenxuantam2611@gmail.com>
2024-11-20 21:51:09 +0700

    update

--- src/router/appointment.routes.js
+++ src/router/appointment.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 import AppointmentController from "../controllers/appointment.controller.js";
 
 const router = express.Router();
-router.get("/", auth, AppointmentController.getListAppointments);
+router.get("/", AppointmentController.getListAppointments);
 router.post("/", AppointmentController.createAppointment);
 router.get("/:id", auth, AppointmentController.getAppointment);
 
