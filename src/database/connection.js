f5229d17aa35292ac7c829e7af11fd83d1c03e0f
thtien03 <118674266+thtien03@users.noreply.github.com>
2024-12-05 00:04:30 +0700

    fix BE

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH, MONGO_DB, MONGO_DB_APP_NAME } =
   process.env;
-console.log(
-  `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
-);
+// // console.log(
+//   `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
+// );
 const connection = mongoose.connect(
   `mongodb://${MONGO_PATH}/${MONGO_DB}`
+  // `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}/${MONGO_DB}`
 );
 
 export default connection;
